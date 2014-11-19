3ec1e2ae4f35fe1d7e55f74d2db4bde5ae38c482
deemsys <sangeetha_d@deemsysinc.com>
2014-11-19 18:44:54 +0530

    conflict recovered
    Conflict in holiday settings view controller rectified

---
+++ ElectronicHealthcareRecord/Holiday_SettingViewController.h
@@ -0,0 +1,30 @@
+//
+//  DoctorHolidayViewController.h
+//  ElectronicHealthcareRecord
+//
+//  Created by DeemsysInc on 18/11/14.
+//  Copyright (c) 2014 Deemsysinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SVSegmentedControl.h"
+#import <QuartzCore/QuartzCore.h>
+#import "UIButton+Bootstrap.h"
+@interface Holiday_SettingViewController : UIViewController
+{
+    UIButton *navButton;
+}
+- (void)segmentedControlChangedValue:(SVSegmentedControl*)segmentedControl;
+
+@property (strong, nonatomic) IBOutlet UIButton *update;
+@property (strong, nonatomic) IBOutlet UIButton *cancel;
+@property (strong, nonatomic) IBOutlet UIButton *reset;
+@property (strong, nonatomic) IBOutlet UIView *personalinfo;
+@property (strong, nonatomic) IBOutlet UIView *workinghours1;
+@property (strong, nonatomic) IBOutlet UIView *workinghours2;
+@property (strong, nonatomic) IBOutlet UIView *holiday1;
+@property (strong, nonatomic) IBOutlet UIView *holiday2;
+@property (strong, nonatomic) IBOutlet UIView *bgview;
+
+
+@end
