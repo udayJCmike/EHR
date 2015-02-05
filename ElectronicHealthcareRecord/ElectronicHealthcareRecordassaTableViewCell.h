9d7dcace9a1a0ac7301e07aeb1d5e2fd97e45f9d
DSYI0016 <uday@Udays-Mac-mini.local>
2015-02-05 18:09:03 +0530

    check

---
+++ ElectronicHealthcareRecord/ElectronicHealthcareRecordassaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ElectronicHealthcareRecordassaTableViewCell.h
+//  ElectronicHealthcareRecord
+//
+//  Created by Uday on 2/5/15.
+//  Copyright (c) 2015 Deemsysinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElectronicHealthcareRecordassaTableViewCell : UITableViewCell
+
+@end
