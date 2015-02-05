f44b18e09f07df3db2a9d0033c839348a32d4f4c
DSYI0016 <uday@Udays-Mac-mini.local>
2015-02-05 18:02:58 +0530

    ahain

---
+++ ElectronicHealthcareRecord/ElectronicHealthcareRecordTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ElectronicHealthcareRecordTableViewCell.h
+//  ElectronicHealthcareRecord
+//
+//  Created by Uday on 2/5/15.
+//  Copyright (c) 2015 Deemsysinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElectronicHealthcareRecordTableViewCell : UITableViewCell
+
+@end
---
+++ ElectronicHealthcareRecord/ElectronicHealthcareRecordTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  ElectronicHealthcareRecordTableViewCell.m
+//  ElectronicHealthcareRecord
+//
+//  Created by Uday on 2/5/15.
+//  Copyright (c) 2015 Deemsysinc. All rights reserved.
+//
+
+#import "ElectronicHealthcareRecordTableViewCell.h"
+
+@implementation ElectronicHealthcareRecordTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
