22b66ee4c07a92850ef2611f2197a78c2f07aecf
DSYI0016 <uday@Udays-Mac-mini.local>
2015-02-05 18:57:47 +0530

    fsdfsfsffdss12212121

---
+++ ElectronicHealthcareRecord/ElectronicHealthcareRecord/testingViewController.h
@@ -0,0 +1,14 @@
+//
+//  testingViewController.h
+//  ElectronicHealthcareRecord
+//
+//  Created by Uday on 2/5/15.
+//  Copyright (c) 2015 Deemsysinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testingViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
