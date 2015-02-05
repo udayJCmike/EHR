ef5560597f8fd5536b4768b047e91c804f4ad703
DSYI0016 <uday@Udays-Mac-mini.local>
2015-02-05 18:44:54 +0530

    dsffdsfds

---
+++ ElectronicHealthcareRecord/ElectronicHealthcareRecord.m
@@ -0,0 +1,34 @@
+//
+//  ElectronicHealthcareRecord.m
+//  ElectronicHealthcareRecord
+//
+//  Created by Uday on 2/5/15.
+//  Copyright (c) 2015 Deemsysinc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ElectronicHealthcareRecord : XCTestCase
+
+@end
+
+@implementation ElectronicHealthcareRecord
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
